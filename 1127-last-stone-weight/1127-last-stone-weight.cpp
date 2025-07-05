c3d0ce26c9553bd1c3afede86da4c6277009d0b6
Vatsal Unadkat <109735785+Vatsaalll@users.noreply.github.com>
2025-07-05 16:09:17 +0530

    Time: 0 ms (100%), Space: 10 MB (45.19%) - LeetHub

---
+++ 1127-last-stone-weight/1127-last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int size = stones.size();
+        int min = INT_MIN;
+        while (size>1) {
+            sort(stones.begin(),stones.end());
+            int last = stones[size-1];
+            int lastsec = stones[size-2];
+            if(last == lastsec) {
+                size -=2;
+            }
+            else {
+                stones[size-2] = last - lastsec;
+                size--;
+            }
+        }
+        if(size==0) return 0;
+        else return stones[0];
+    }
+};
