5d053158a9743d0c086ce56570a08af2833b06d8
Vatsal Unadkat <109735785+Vatsaalll@users.noreply.github.com>
2025-09-04 11:37:47 +0530

    Time: 0 ms (100%), Space: 8.7 MB (0.68%) - LeetHub

---
+++ 3830-find-closest-person/3830-find-closest-person.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findClosest(int x, int y, int z) {
+        int one = abs(x-z);
+        int two = abs(y-z);
+        if(one>two) {
+            return 2;
+        }
+        else if(one<two) {
+            return 1;
+        }
+        return 0;
+    }
+};
